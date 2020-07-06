67954848ba64823dbf312fe6e410032bc71e6a3e
Torii <toritaku10@gmail.com>
2020-07-06 11:00:25 +0000

    modify

--- scripts/model/tf_util/__init__.py
+++ scripts/model/tf_util/__init__.py
@@ -1,7 +1,7 @@
 #! -*- coding:utf-8 -*-
 
 from .batch_normalize import batch_norm
-from .variable_util import get_const_variable, get_rand_variable, flatten, get_dim
+from .variable_util import get_const_variable, get_rand_variable, flatten, get_dim, get_channel
 from .lrelu import lrelu
 from .linear import linear, sn_linear, const_linear
 from .conv import conv, sn_conv
